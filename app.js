d1c8ab6bafac05e8aea88b8866e878431d36b5da
UsaidRashid <rashidusaid2@gmail.com>
2024-06-27 16:30:41 +0530

    Few Updates

--- app.js
+++ app.js
@@ -67,6 +67,10 @@ app.use((req,res,next) => {
     next();
 });
 
+app.get('/', (req, res) => {    
+    res.render('index.ejs');
+});
+
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
